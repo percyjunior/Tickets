9a9a87a0dc06a6d8fa3d924e8a82f05116d11ddd
Percy Gamboa <percy.gamboa@infosimsolutions.com>
2019-05-07 04:32:54 -0400

    agregado de formularios

--- controller/pedido.js
+++ controller/pedido.js
@@ -16,3 +16,33 @@ exports.eventoDetallePost = function(req, res, next) {
 exports.asientoDetalle = function(req, res, next){
     res.render('Evento/seleccionAsientos');
 }
+
+exports.asientoDetallePost = function(req, res, next){
+    item = {
+        cantiAdu: req.body.cantiAdu,
+        cantikid: req.body.cantikid,
+        precioAdu: req.body.precioAdu,
+        precioKid: req.body.precioKid
+    }
+    console.log("Esto son lo precios y las cantidades para los boletos:");
+    console.log(item);
+    res.redirect('/evento/formulario');
+}
+
+exports.formularioCliente = function(req, res, next){
+    res.render('Evento/formularioPersonas');
+}
+
+exports.formularioClientePost = function(req, res, next){
+    item = {
+        ci1: req.body.ci1,
+        nombre1: req.body.nombre1,
+        nacimiento1: req.body.cnacimiento,
+        ci2: req.body.ci2,
+        nombre2: req.body.nombre2,
+        nacimiento2: req.body.nacimiento2,
+        email: req.body.email
+    }
+    console.log(item);
+    res.redirect('/');
+}
