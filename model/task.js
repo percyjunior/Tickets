0272a5f6b251fd2cee524d8f01cbc81c2207504f
anemixydnew <wendycalizayaperez@gmail.com>
2019-05-28 15:55:30 -0400

    Enviar mensaje al Email actualizado, con campos ajustados y aumentando telefono, nombre, apellidos, sexo

--- model/task.js
+++ model/task.js
@@ -5,10 +5,16 @@ lon = 6;
 const TaskSchema= Schema({
     ci1: String,
     nombre1:String,
+    apellido1:String,
+    sexo1:String,
+    telefono1:String,
     nacimiento1: String,
     a:String,
     ci2:String,
     nombre2:String,
+    apellido2:String,
+    sexo2:String,
+    telefono2:String,
     nacimiento2: String,
     email:String,
   status: {
