75a73e238e7155733633b37e94868d138a9e3482
kevinchuca <Diego_dmp15@gmail.com>
2019-06-12 18:37:17 -0400

    hice reportes y haya errores kevinchuca bozo vargas

--- public/app/app.js
+++ public/app/app.js
@@ -14,7 +14,10 @@ var app = $.sammy(function () {
         loadPage('ticket/mostrar')
         //your function
     });
-   
+    this.get('#/Evento/Reportes', function () {
+        loadPage('Reportes/mostrar')
+        //your function
+    });
     this.get('#/registros', function () {
         loadPage('registro/home')
         //your function
