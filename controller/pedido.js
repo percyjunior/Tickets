83c780997a61a06694b31309288e59e6f7695bf7
anemixydnew <wendycalizayaperez@gmail.com>
2019-06-14 04:14:32 -0400

    algunas interfaces

--- controller/pedido.js
+++ controller/pedido.js
@@ -127,7 +127,6 @@ function email(respuesta, ci1, nombre1,apellido1, sexo1,telefono1,nacimiento1, c
 }
 
 exports.formularioClientePost = function (req, res, next) {
-  console.log("guardo persona");
     var cod = ["cod1", "cod2"];
     /* for (var j = 0; j < (cod[0]+cod[1]); j++) {
          var listItem = req.body.createElement('li');
@@ -158,25 +157,20 @@ exports.formularioClientePost = function (req, res, next) {
         ],
         codigo:a
     }
-    console.log(req.body);
-    console.log(req.tickets);
-
-    for(i in req.data){
-      task = new Task(req.data[i]);
-      console.log("entro al for");
-
+    for(i in item.data){
+      task = new Task(item.data[i]);
       task.save();
     }
     /*task = new Task(item);
    task.save();*/
-    for (i in req.data) {
-        let datastring = JSON.stringify(req.data[i]);
+    for (i in item.data) {
+        let datastring = JSON.stringify(item.data[i]);
         qrImage
             .image(datastring, { type: 'jpg', size: 20 })
-            .pipe(fs.createWriteStream(req.data[i].ci + ".jpg"));
-        console.log(req.data[i]);
+            .pipe(fs.createWriteStream(item.data[i].ci + ".jpg"));
+        console.log(item.data[i]);
     }
-   /* email(res, req.body.ci1, req.body.nombre1,req.body.apellido1,req.body.sexo1,req.body.telefono1, req.body.nacimiento1
+    email(res, req.body.ci1, req.body.nombre1,req.body.apellido1,req.body.sexo1,req.body.telefono1, req.body.nacimiento1
       , req.body.ci2, req.body.nombre2, req.body.apellido2,req.body.sexo2,req.body.telefono2,req.body.nacimiento2, req.body.email,a);  
       
       const create_payment_json = {
@@ -217,5 +211,5 @@ exports.formularioClientePost = function (req, res, next) {
           }
         }
       });
-    */
+    
 }
